f1aec78e1b71cee75aabe9d99268b8f91e426c87
ABHIJIT\abhij <gadakhabhijit@gmail.com>
2024-09-25 18:03:20 +0530

    removed write-up and added resume download functionality

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  downloadPDF() {
+    const link = document.createElement('a');
+    link.href = 'assets/abhijit_gadakh_resume.pdf'; // Relative path to your PDF in the assets folder
+    link.download = 'abhijit_gadakh_resume.pdf'; // The name to give the downloaded file
+    link.click();
+    window.open('assets/abhijit_gadakh_resume.pdf', '_blank');
   }
-
 }
